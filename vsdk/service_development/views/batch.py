43112565df1a6db50e9f25f848e2811b1008da16
rvrheenen <rick.van.rheenen@gmail.com>
2018-05-27 19:40:20 +0200

    yet another batch submission bug fix

--- vsdk/service_development/views/batch.py
+++ vsdk/service_development/views/batch.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from django.core.exceptions import ObjectDoesNotExist
 from datetime import datetime, timedelta
 
-from ..models import KasaDakaUser, CallSession, Language, Disease, VoiceLabel
+from ..models import KasaDakaUser, CallSession, Language, Disease, VoiceLabel, Batch
 
 from . import base
 
